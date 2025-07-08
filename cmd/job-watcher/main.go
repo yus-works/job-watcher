9e7d9811046b44401b345fa5e16a5fce5c32472f
teodord25 <djuric.teodor25@gmail.com>
2025-07-08 15:35:17 +0200

    basic http setup

--- cmd/job-watcher/main.go
+++ cmd/job-watcher/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
 
 func main() {
-	fmt.Println("hello world");
+	fmt.Println("hello world")
+
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8080", nil)
 }
