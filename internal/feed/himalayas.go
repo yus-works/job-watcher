fdc7822e6336935affbff5e553d3d4717a582c60
teodord25 <djuric.teodor25@gmail.com>
2025-07-13 16:07:56 +0200

    add: himalayas details

---
+++ internal/feed/himalayas.go
@@ -0,0 +1,13 @@
+package feed
+
+/* 
+url: https://himalayas.app/jobs/api
+type: JSON 
+options: offset, limit
+details:
+- limit max is 20
+- ordered most recent to least recent
+- offset is offset not page number
+- rate limited
+info: https://himalayas.app/api
+*/
