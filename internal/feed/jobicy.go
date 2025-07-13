41a87b9a19a9b7f97fe17028d6b97cbe2ad29cfa
teodord25 <djuric.teodor25@gmail.com>
2025-07-13 16:15:04 +0200

    add: jobicy details

---
+++ internal/feed/jobicy.go
@@ -0,0 +1,12 @@
+package feed
+
+/* 
+url: https://jobicy.com/api/v2/remote-jobs
+type: JSON 
+options: count, geo, industry, tag
+details:
+- count max is 100
+- ordered most recent to least recent
+- rate limited
+info: https://jobicy.com/jobs-rss-feed
+*/
