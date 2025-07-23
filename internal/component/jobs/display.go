6770d62a6e4a7669d9dcf7f4fe1b400ef999fb30
teodord25 <djuric.teodor25@gmail.com>
2025-07-23 06:59:00 +0200

    feat(display-item): add seniority

--- internal/component/jobs/display.go
+++ internal/component/jobs/display.go
@@ -7,17 +7,19 @@ import (
 
 type DisplayItem struct {
 	feed.Item
-	JobType string
-	Date    string
-	Age     string
+	Seniority string
+	JobType   string
+	Date      string
+	Age       string
 }
 
 func NewDisplayItem(i feed.Item) DisplayItem {
 	return DisplayItem{
 		Item: i,
 
-		JobType: string(i.JobType),
-		Date:    i.Date.Local().Format("2006-01-02"),
-		Age:     parser.HumanAgeGreedy(i.Age),
+		Seniority: string(i.Seniority),
+		JobType:   string(i.JobType),
+		Date:      i.Date.Local().Format("2006-01-02"),
+		Age:       parser.HumanAgeGreedy(i.Age),
 	}
 }
