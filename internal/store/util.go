4404e9b3754ada18a97bc98dda0a481a7ef22660
teodord25 <djuric.teodor25@gmail.com>
2025-07-27 11:34:22 +0200

    feat(store): impl normalization and hash functions

---
+++ internal/store/util.go
@@ -0,0 +1,30 @@
+package store
+
+import (
+	"hash/fnv"
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/unicode/norm"
+)
+
+func Normalize(s string) string {
+	s = strings.TrimSpace(s)
+	s = cases.Fold().String(s)
+	t := norm.NFKD.String(s)
+	buf := make([]rune, 0, len(t))
+	for _, r := range t {
+		if unicode.Is(unicode.Mn, r) {
+			continue
+		}
+		buf = append(buf, r)
+	}
+	return strings.Join(strings.Fields(string(buf)), " ")
+}
+
+func HashNormalized64(s string) uint64 {
+	h := fnv.New64a()
+	h.Write([]byte(Normalize(s)))
+	return h.Sum64()
+}
