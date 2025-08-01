705fe0a760708bae70cecb753e1cfb35fb512dbe
teodord25 <djuric.teodor25@gmail.com>
2025-08-01 23:27:55 +0200

    chore(home): rm redundant getJobs call

--- internal/component/home/home.go
+++ internal/component/home/home.go
@@ -1,10 +1,8 @@
 package home
 
 import (
-	"context"
 	"html/template"
 	"net/http"
-	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/yus-works/job-watcher/internal/store"
@@ -12,20 +10,7 @@ import (
 
 func Register(log *logrus.Entry, tl *template.Template, st *store.JobStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		ctx, cancel := context.WithTimeout(req.Context(), 3*time.Second)
-		defer cancel()
-
-		// TODO: this seems redundant
-		jobs, err := st.GetJobs(log, ctx, req.URL.Query().Get("search"))
-		if err != nil {
-			http.Error(w, "timeout or db error", 500)
-			log.WithFields(logrus.Fields{
-				"err": err,
-			}).Error("get jobs")
-			return
-		}
-
-		err = tl.ExecuteTemplate(w, "home", jobs)
+		err := tl.ExecuteTemplate(w, "home", nil)
 		if err != nil {
 			log.WithFields(logrus.Fields{
 				"name": "home",
