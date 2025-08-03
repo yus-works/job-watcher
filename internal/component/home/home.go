3270f5c4afc2fd05c8e3043e0b9171637b2c767f
teodord25 <djuric.teodor25@gmail.com>
2025-08-03 18:49:06 +0200

    feat(home): add ticking last refresh since ago

--- internal/component/home/home.go
+++ internal/component/home/home.go
@@ -3,6 +3,7 @@ package home
 import (
 	"html/template"
 	"net/http"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/yus-works/job-watcher/internal/store"
@@ -10,8 +11,19 @@ import (
 
 func Register(log *logrus.Entry, tl *template.Template, st *store.JobStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		err := tl.ExecuteTemplate(w, "home", nil)
-		if err != nil {
+		last, _ := st.LastRefresh(req.Context())
+		data := struct {
+			LastRefreshISO string
+		}{
+			LastRefreshISO: func() string {
+				if last.IsZero() {
+					return ""
+				}
+				return last.UTC().Format(time.RFC3339Nano)
+			}(),
+		}
+
+		if err := tl.ExecuteTemplate(w, "home", data); err != nil {
 			log.WithFields(logrus.Fields{
 				"name": "home",
 			}).WithError(err).Error("Failed to execute template")
