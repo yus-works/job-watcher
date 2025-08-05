1fd05461ae9660c887cacd7710781762118cdbb2
teodord25 <djuric.teodor25@gmail.com>
2025-08-05 16:09:13 +0200

    chore(middleware): rm unused

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,11 +1,7 @@
 package middleware
 
 import (
-	"log/slog"
 	"net/http"
-
-	"github.com/rs/xid"
-	"github.com/yus-works/job-watcher/internal/logging"
 )
 
 type Middleware = func(http.Handler) http.Handler
@@ -15,13 +11,4 @@ func Chain(h http.Handler, mws ...Middleware) http.Handler {
 		h = mws[i](h)
 	}
 	return h
-}
-
-func WithRequestLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		reqID := xid.New().String()
-		l := slog.Default().With("req_id", reqID, "path", r.URL.Path)
-		ctx := logging.Into(r.Context(), l)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
 }
